47da0225260be1be3f76f7ed2d2abd598601ac5a
Bobby Thompson <bobbythompson@paypoint.com>
2024-10-19 23:59:55 +0100

    Started

---
+++ src/AiTrainer.Web/AiTrainer.Web.Workflow/Activities/Abstract/IActivity.cs
@@ -0,0 +1,10 @@
+﻿using AiTrainer.Web.Workflow.Models;
+
+namespace AiTrainer.Web.Workflow.Activities.Abstract
+{
+    public interface IActivity
+    {
+        public string Description {  get; }
+        Task<ActivityResultEnum> ExecuteAsync();
+    }
+}
---
+++ src/AiTrainer.Web/AiTrainer.Web.Workflow/Models/CompletedActivityResult.cs
@@ -0,0 +1,12 @@
+﻿using AiTrainer.Web.Workflow.Activities.Abstract;
+
+namespace AiTrainer.Web.Workflow.Models
+{
+    public record CompletedActivityResult
+    {
+        public IActivity ActualActivity { get; init; }
+        public ActivityResultEnum ActivityResultEnum { get; init; }
+        public DateTime HappenAt { get; init; }
+        public TimeSpan TimeTaken { get; init; }
+    }
+}
