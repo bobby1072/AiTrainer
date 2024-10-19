646a0810d7521287ca6ee302d7e5b0f82d41de5c
bobby1072 <bobby.thompson1072@gmail.com>
2024-10-19 03:24:40 +0100

    Pipeline fix

--- src/AiTrainer.Web/AiTrainer.Web.Persistence/EntityFramework/Entities/UserEntity.cs
+++ src/AiTrainer.Web/AiTrainer.Web.Persistence/EntityFramework/Entities/UserEntity.cs
@@ -1,15 +1,20 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using AiTrainer.Web.Common.Models.DomainModels;
 
 namespace AiTrainer.Web.Persistence.EntityFramework.Entities
 {
     [Table("user", Schema = DbConstants.PublicSchema)]
-    internal record UserEntity : BaseEntity<UserEntity>
+    internal record UserEntity : BaseEntity<User>
     {
         [Key]
         public Guid Id { get; set; }
         public string Email { get; set; }
         public string? Name { get; set; }
         public string Username { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+
+        public override User ToModel() => new(Email, Username, DateCreated, DateModified, Name, Id);
     }
 }
