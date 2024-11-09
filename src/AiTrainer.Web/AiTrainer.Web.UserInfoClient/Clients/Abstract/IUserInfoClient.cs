75a2959e79498bc9e33f48ca6e54620ffa0a3a40
bobby1072 <bobby.thompson1072@gmail.com>
2024-11-09 01:55:26 +0000

    Starting over

--- src/AiTrainer.Web/AiTrainer.Web.UserInfoClient/Clients/Abstract/IUserInfoClient.cs
+++ src/AiTrainer.Web/AiTrainer.Web.UserInfoClient/Clients/Abstract/IUserInfoClient.cs
@@ -4,6 +4,7 @@ namespace AiTrainer.Web.UserInfoClient.Clients.Abstract
 {
     public interface IUserInfoClient
     {
+        Task<UserInfoResponse> InvokeAsync(string accessToken);
         Task<UserInfoResponse?> TryInvokeAsync(string accessToken);
     }
 }
