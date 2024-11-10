c20b4822b12faf365c863481d06c73486f795e88
bobby1072 <bobby.thompson1072@gmail.com>
2024-11-10 23:36:40 +0000

    Client updates

--- src/AiTrainer.Web/AiTrainer.Web.CoreClient/Clients/Abstract/ICoreClient.cs
+++ src/AiTrainer.Web/AiTrainer.Web.CoreClient/Clients/Abstract/ICoreClient.cs
@@ -1,14 +1,17 @@
-﻿namespace AiTrainer.Web.CoreClient.Clients.Abstract
+﻿using AiTrainer.Web.CoreClient.Models.Request;
+using AiTrainer.Web.CoreClient.Models.Response;
+
+namespace AiTrainer.Web.CoreClient.Clients.Abstract
 {
     public interface ICoreClient<TParam, TReturn>
-        where TParam : class
-        where TReturn : class
+        where TParam : BaseCoreClientRequestBody
+        where TReturn : BaseCoreClientResponseBody
     {
         Task<TReturn?> TryInvokeAsync(TParam param);
     }
 
     public interface ICoreClient<TReturn>
-        where TReturn : class
+        where TReturn : BaseCoreClientResponseBody
     {
         Task<TReturn?> TryInvokeAsync();
     }
