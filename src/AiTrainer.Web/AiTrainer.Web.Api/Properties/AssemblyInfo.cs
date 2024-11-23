a5f9596e658c152ed33fe2460916d4b9de6d92d1
bobby1072 <bobby.thompson1072@gmail.com>
2024-11-23 22:49:43 +0000

    New try

--- src/AiTrainer.Web/AiTrainer.Web.Api/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿[assembly: System.Reflection.AssemblyCompany("AiTrainer.Web.Api")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersion(
-    "1.0.0+abb2ed20b9fb1589118e585124855126b6f02b1c"
-)]
-[assembly: System.Reflection.AssemblyProduct("AiTrainer.Web.Api")]
-[assembly: System.Reflection.AssemblyTitle("AiTrainer.Web.Api")]
-[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
