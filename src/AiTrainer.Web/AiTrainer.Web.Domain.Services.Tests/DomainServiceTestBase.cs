e35bc127d7c722e647c309717da16b2295c64c44
Bobby Thompson <bobbythompson@paypoint.com>
2024-12-10 09:49:46 +0000

    Test fix

--- src/AiTrainer.Web/AiTrainer.Web.Domain.Services.Tests/DomainServiceTestBase.cs
+++ src/AiTrainer.Web/AiTrainer.Web.Domain.Services.Tests/DomainServiceTestBase.cs
@@ -9,7 +9,6 @@ namespace AiTrainer.Web.Domain.Services.Tests
     public abstract class DomainServiceTestBase
     {
         protected readonly Fixture _fixture = new();
-        protected readonly Mock<IHttpContextAccessor> _mockHttpContextAccessor = new();
         protected readonly Mock<HttpContext> _mockHttpContext = new();
         protected readonly Mock<HttpRequest> _mockHttpRequest = new();
         protected readonly Mock<HttpResponse> _mockHttpResponse = new();
@@ -24,10 +23,9 @@ namespace AiTrainer.Web.Domain.Services.Tests
             _mockHttpContext.Setup(x => x.Response).Returns(_mockHttpResponse.Object);
             _mockHttpContext.Setup(x => x.Request.Headers).Returns(_headerDictionary);
             _mockHttpContext.Setup(x => x.Response.Headers).Returns(_headerDictionary);
-            _mockHttpContextAccessor.Setup(x => x.HttpContext).Returns(_mockHttpContext.Object);
 
             _mockApiRequestService = new ApiRequestHttpContextService(
-                _mockHttpContextAccessor.Object
+                _mockHttpContext.Object
             );
         }
     }
