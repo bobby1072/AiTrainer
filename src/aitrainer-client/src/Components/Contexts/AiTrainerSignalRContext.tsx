f42c021045d126fee24d700f43f239878afd4ec4
bobby1072 <bobby.thompson1072@gmail.com>
2025-01-09 01:28:07 +0000

    More small changes

--- src/aitrainer-client/src/Components/Contexts/AiTrainerSignalRContext.tsx
+++ src/aitrainer-client/src/Components/Contexts/AiTrainerSignalRContext.tsx
@@ -1,3 +1,37 @@
-import { createContext } from "react";
+import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
+import { createContext, useEffect, useState } from "react";
+import AppSettingsProvider from "../../Utils/AppSettingsProvider";
+import { AppSettingsKeys } from "../../Utils/AppSettingsKeys";
+import { Loading } from "../Common/Loading";
 
-export const AiTrainerSignalRContext = createContext<undefined>(undefined);
+const signalRConnectionBuilder = new HubConnectionBuilder().withUrl(
+  `${
+    AppSettingsProvider.TryGetValue(AppSettingsKeys.AiTrainerWebEndpoint) ??
+    "http://localhost:5222"
+  }/Api/SignalR`
+);
+
+export type AiTrainerSignalRContextType = {
+  hubConnection: HubConnection;
+};
+
+export const AiTrainerSignalRContext = createContext<
+  AiTrainerSignalRContextType | undefined
+>(undefined);
+
+export const AiTrainerSignalRProvider: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => {
+  const [hubConnection, setHubConnection] = useState<HubConnection>();
+
+  useEffect(() => {
+    setHubConnection(signalRConnectionBuilder.build());
+  }, [setHubConnection]);
+
+  if (!hubConnection) return <Loading />;
+  return (
+    <AiTrainerSignalRContext.Provider value={{ hubConnection }}>
+      {children}
+    </AiTrainerSignalRContext.Provider>
+  );
+};
