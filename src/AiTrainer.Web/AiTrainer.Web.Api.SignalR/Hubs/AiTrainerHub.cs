c0ef77fab4d34f84ac32c73cd94d5ed2c5820956
bobby1072 <bobby.thompson1072@gmail.com>
2025-01-10 23:16:43 +0000

    Readd user stuff

--- src/AiTrainer.Web/AiTrainer.Web.Api.SignalR/Hubs/AiTrainerHub.cs
+++ src/AiTrainer.Web/AiTrainer.Web.Api.SignalR/Hubs/AiTrainerHub.cs
@@ -1,22 +1,56 @@
+using System.Net;
+using AiTrainer.Web.Common.Attributes;
+using AiTrainer.Web.Common.Exceptions;
 using AiTrainer.Web.Common.Extensions;
+using AiTrainer.Web.Domain.Services.Abstract;
+using AiTrainer.Web.Domain.Services.User.Abstract;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
 
 namespace AiTrainer.Web.Api.SignalR.Hubs
 {
+    [RequireUserLogin]
     public class AiTrainerHub : Hub
     {
+        public const string ConnectionIdUserIdCacheKeyPrefix = "userId-";
+        private readonly IUserProcessingManager _userProcessingManager;
         private readonly ILogger<AiTrainerHub> _logger;
-        public AiTrainerHub(ILogger<AiTrainerHub> logger)
+        private readonly ICachingService _cachingService;
+
+        public AiTrainerHub(
+            IUserProcessingManager userProcessingManager,
+            ILogger<AiTrainerHub> logger,
+            ICachingService cachingService
+        )
         {
+            _userProcessingManager = userProcessingManager;
             _logger = logger;
+            _cachingService = cachingService;
         }
+
         public override async Task OnConnectedAsync()
         {
             await base.OnConnectedAsync();
-            var correlationId = Context.GetHttpContext()?.GetCorrelationId();
-            
-            _logger.LogInformation("Client connected with connectionId {ConnectionId} and correlationId {CorrelationId}", Context.ConnectionId, correlationId);
+
+            var hubHttpContext = Context.GetHttpContext();
+            var userAccessToken = hubHttpContext.GetAccessToken();
+
+            var foundCachedUser =
+                await _userProcessingManager.TryGetUserFromCache(userAccessToken)
+                ?? throw new ApiException("Can't find user", HttpStatusCode.Unauthorized);
+
+            await _cachingService.SetObject(
+                $"{ConnectionIdUserIdCacheKeyPrefix}{foundCachedUser.Id}",
+                Context.ConnectionId
+            );
+
+            var correlationId = hubHttpContext?.GetCorrelationId();
+
+            _logger.LogInformation(
+                "Client connected with connectionId {ConnectionId} and correlationId {CorrelationId}",
+                Context.ConnectionId,
+                correlationId
+            );
         }
     }
 }
