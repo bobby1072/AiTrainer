c4cfd63a7b4d9251c289610daa28d57c20bc7db2
bobby1072 <bobby.thompson1072@gmail.com>
2025-01-20 19:32:37 +0000

    Fixed build scirpt

--- build-scripts/create_express_app_settings_json.ps1
+++ build-scripts/create_express_app_settings_json.ps1
@@ -1,13 +1,13 @@
 # Define the data as a hashtable
 $data = @{
-    OPENAI_API_KEY  = "<OpenAI_ApiKey>"
+    OPENAI_API_KEY  = "OpenAI_ApiKey"
     DocumentChunker = @{
         ChunkSize    = 512
         ChunkOverlap = 128
     }
     ExpressPort     = 4000
     AiTrainerCore   = @{
-        ApiKey = "<AiTrainer_ApiKey>"
+        ApiKey = "AiTrainer_ApiKey"
     }
 }
 
@@ -15,7 +15,7 @@ $data = @{
 $json = $data | ConvertTo-Json -Depth 3 -Compress
 
 # Save the JSON to a file
-$json | Out-File -FilePath "src/AiTrainer.Core/src/Data/expressappsettings.json" -Encoding utf8
+$json | Out-File -FilePath "./../src/AiTrainer.Core/src/Data/expressappsettings.json" -Encoding utf8
 
 # Output to console for confirmation
-Write-Host "JSON file 'config.json' has been created."
+Write-Host "JSON file 'expressappsettings.json' has been created."
