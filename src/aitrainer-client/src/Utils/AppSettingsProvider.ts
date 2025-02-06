4c3850ae1316ebe81724cd298dfbbc594ddc8b86
Bobby Thompson <bobbythompson@paypoint.com>
2025-02-06 10:41:39 +0000

    Donezo

--- src/aitrainer-client/src/Utils/AppSettingsProvider.ts
+++ src/aitrainer-client/src/Utils/AppSettingsProvider.ts
@@ -1,11 +1,11 @@
 const appSettingsJson: Record<
   string,
   any
-> = require("./../data/expressappsettings.json");
+> = require("./../Data/reactappsettings.json");
 const appSettingsDevJson: Record<
   string,
   any
-> = require("./../data/expressappsettings.dev.json");
+> = require("./../Data/reactappsettings.dev.json");
 
 const isForceProduction: boolean = appSettingsJson["UseProd"];
 
