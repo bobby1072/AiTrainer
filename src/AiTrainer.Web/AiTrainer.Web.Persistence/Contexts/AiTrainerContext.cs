c745cc7aeac92f402331831e469edf00ec376a8e
bobby1072 <bobby.thompson1072@gmail.com>
2025-03-03 21:47:05 +0000

    Made chnzages

--- src/AiTrainer.Web/AiTrainer.Web.Persistence/Contexts/AiTrainerContext.cs
+++ src/AiTrainer.Web/AiTrainer.Web.Persistence/Contexts/AiTrainerContext.cs
@@ -1,3 +1,4 @@
+using AiTrainer.Web.Domain.Models;
 using AiTrainer.Web.Persistence.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,7 +17,6 @@ namespace AiTrainer.Web.Persistence.Contexts
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
             modelBuilder.Entity<FileDocumentMetaDataEntity>(ent =>
             {
                 ent.ToTable("file_document_metadata");
@@ -55,8 +55,7 @@ namespace AiTrainer.Web.Persistence.Contexts
             });
             modelBuilder.Entity<FileCollectionFaissEntity>(ent =>
             {
-                ent
-                    .HasOne<FileCollectionEntity>()
+                ent.HasOne<FileCollectionEntity>()
                     .WithOne(x => x.FaissStore)
                     .HasForeignKey<FileCollectionFaissEntity>(x => x.CollectionId);
             });
@@ -87,5 +86,79 @@ namespace AiTrainer.Web.Persistence.Contexts
                     .HasConstraintName("fk_file_document_collection_id");
             });
         }
+
+        public override async Task<int> SaveChangesAsync(
+            CancellationToken cancellationToken = default
+        )
+        {
+            var updatingEntries = ChangeTracker.Entries()
+                .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+
+            foreach (var entry in updatingEntries)
+            {
+                if (entry.Entity is UserEntity foundUser)
+                {
+                    if (entry.State == EntityState.Added)
+                    {
+                        UpdateEntityDatesToToday<UserEntity, Guid, User>(foundUser, [nameof(UserEntity.DateCreated), nameof(UserEntity.DateModified)]);
+                    }
+                    else
+                    {
+                        UpdateEntityDatesToToday<UserEntity, Guid, User>(foundUser, [nameof(UserEntity.DateModified)]);
+                    }
+                }
+                else if (entry.Entity is FileCollectionEntity foundCollection)
+                {
+                    if (entry.State == EntityState.Added)
+                    {
+                        UpdateEntityDatesToToday<FileCollectionEntity, Guid, FileCollection>(foundCollection, [nameof(FileCollectionEntity.DateCreated), nameof(FileCollectionEntity.DateModified)]);
+                    }
+                    else
+                    {
+                        UpdateEntityDatesToToday<FileCollectionEntity, Guid, FileCollection>(foundCollection, [nameof(FileCollectionEntity.DateCreated)]);
+                    }
+                }
+                else if (entry.Entity is FileDocumentEntity foundDocument)
+                {
+                    if (entry.State == EntityState.Added)
+                    {
+                        UpdateEntityDatesToToday<FileDocumentEntity, Guid, FileDocument>(foundDocument, [nameof(FileDocumentEntity.DateCreated)]);
+                    }
+                }
+            }
+            
+            
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+
+        private static void UpdateEntityDatesToToday<TEnt, TId, TRuntime>(TEnt ent, IReadOnlyCollection<string> propertyNames) 
+            where TEnt : BaseEntity<TId, TRuntime>
+            where TRuntime : class
+        {
+            foreach (var dateType in propertyNames)
+            {
+                try
+                {
+                    UpdateObjectDatesToToday(ent, dateType);
+                }
+                catch
+                {
+                    //This is ok because we are just trying to update values
+                }
+            }
+        }
+
+        private static void UpdateObjectDatesToToday<TEnt>(TEnt ent, string propertyName)
+        {
+            var propertyToUpdate = typeof(TEnt).GetProperty(propertyName);
+            if (propertyToUpdate == null || propertyToUpdate.PropertyType != typeof(DateTime))
+            {
+                return;
+            }
+            
+            propertyToUpdate.SetValue(ent, DateTime.UtcNow);
+        }
     }
+    
 }
