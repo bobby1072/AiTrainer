12f5a892d100185cbaa41ddf5e057371cb38ba36
bobby1072 <bobby.thompson1072@gmail.com>
2025-04-24 01:01:45 +0100

    Started

---
+++ src/AiTrainer.Core/src/OpenAI/ChatQueryService.test.ts
@@ -0,0 +1,6 @@
+import { describe, it } from "@jest/globals";
+import ChatQueryService from "./ChatQueryService";
+
+describe(`${ChatQueryService.name}`, () => {
+  it(`${ChatQueryService.name} `, () => {});
+});
