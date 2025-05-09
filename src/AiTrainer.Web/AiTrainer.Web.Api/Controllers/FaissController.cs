d05725f4624615c92dbecc570dd735aaf7688d24
bobby1072 <bobby.thompson1072@gmail.com>
2025-05-09 02:02:40 +0100

    Added

--- src/AiTrainer.Web/AiTrainer.Web.Api/Controllers/FaissController.cs
+++ src/AiTrainer.Web/AiTrainer.Web.Api/Controllers/FaissController.cs
@@ -11,9 +11,10 @@ using Microsoft.AspNetCore.Mvc;
 namespace AiTrainer.Web.Api.Controllers;
 
 [RequireUserLogin]
-public class FaissController: BaseController
+public class FaissController : BaseController
 {
     private readonly IFileCollectionFaissSyncBackgroundJobQueue _faissSyncBackgroundJobQueue;
+
     public FaissController(
         IHttpDomainServiceActionExecutor actionExecutor,
         IFileCollectionFaissSyncBackgroundJobQueue faissSyncBackgroundJobQueue
@@ -24,37 +25,43 @@ public class FaissController: BaseController
     }
 
     [HttpPost("Chat/Query")]
-    public async Task<ActionResult<Outcome<string>>> ChatQuery([FromBody] ChatGptFormattedQueryInput input, CancellationToken ct = default)
+    public async Task<ActionResult<Outcome<string>>> ChatQuery(
+        [FromBody] ChatGptFormattedQueryInput input,
+        CancellationToken ct = default
+    )
     {
         var currentUser = await GetCurrentUser();
 
-        var result = await _actionExecutor
-            .ExecuteAsync<IChatGptQueryProcessingManager, string>(serv => serv.ChatGptFaissQuery(input, currentUser, ct), nameof(IChatGptQueryProcessingManager.ChatGptFaissQuery));
-        
-        return new Outcome<string>
-        {
-            Data = result
-        };
+        var result = await _actionExecutor.ExecuteAsync<IChatGptQueryProcessingManager, string>(
+            serv => serv.ChatGptFaissQuery(input, currentUser, ct),
+            nameof(IChatGptQueryProcessingManager.ChatGptFaissQuery)
+        );
+
+        return new Outcome<string> { Data = result };
     }
 
     [HttpPost("Sync")]
-    public async Task<ActionResult<Outcome>> SyncFaissStore([FromBody] SyncFaissStoreHubInput input, CancellationToken ct = default)
+    public async Task<ActionResult<Outcome>> SyncFaissStore(
+        [FromBody] SyncFaissStoreHubInput input,
+        CancellationToken ct = default
+    )
     {
         var currentUser = await GetCurrentUser();
 
-        await _actionExecutor.ExecuteAsync<IFileCollectionFaissSyncProcessingManager>(serv =>
-            serv.SyncUserFileCollectionFaissStore(
-                currentUser,
-                input.CollectionId,
-                false,
-                ct
-            ), nameof(IFileCollectionFaissSyncProcessingManager.SyncUserFileCollectionFaissStore)
+        await _actionExecutor.ExecuteAsync<IFileCollectionFaissSyncProcessingManager>(
+            serv =>
+                serv.SyncUserFileCollectionFaissStore(currentUser, input.CollectionId, false, ct),
+            nameof(IFileCollectionFaissSyncProcessingManager.SyncUserFileCollectionFaissStore)
         );
 
         return new Outcome();
     }
+
     [HttpPost("TriggerSync")]
-    public async Task<ActionResult<Outcome>> TriggerSyncFaissStore([FromBody] SyncFaissStoreHubInput input, CancellationToken ct = default)
+    public async Task<ActionResult<Outcome>> TriggerSyncFaissStore(
+        [FromBody] SyncFaissStoreHubInput input,
+        CancellationToken ct = default
+    )
     {
         var currentUser = await GetCurrentUser();
 
@@ -63,25 +70,30 @@ public class FaissController: BaseController
             {
                 CollectionId = input.CollectionId,
                 CurrentUser = currentUser,
-                RetryOverride = false
+                RetryOverride = false,
             },
             ct
         );
 
-
         return new Outcome();
     }
+
     [HttpPost("SimilaritySearch")]
     public async Task<Outcome<IReadOnlyCollection<SingleDocumentChunk>>> SimilaritySearchFaissStore(
-        [FromBody] SimilaritySearchInput input, CancellationToken ct = default)
+        [FromBody] SimilaritySearchInput input,
+        CancellationToken ct = default
+    )
     {
         var currentUser = await GetCurrentUser();
-        
+
         var result = await _actionExecutor.ExecuteAsync<
             IFileCollectionFaissSimilaritySearchProcessingManager,
             IReadOnlyCollection<SingleDocumentChunk>
-        >(serv => serv.SimilaritySearch(input, currentUser, ct), nameof(IFileCollectionFaissSimilaritySearchProcessingManager.SimilaritySearch)); 
-        
+        >(
+            serv => serv.SimilaritySearch(input, currentUser, ct),
+            nameof(IFileCollectionFaissSimilaritySearchProcessingManager.SimilaritySearch)
+        );
+
         return new Outcome<IReadOnlyCollection<SingleDocumentChunk>> { Data = result };
     }
 }
