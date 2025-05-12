85012603e2aabf4e44fb31604bda401461d7c90d
bobby1072 <bobby.thompson1072@gmail.com>
2025-05-12 23:12:16 +0100

    Should work

--- src/AiTrainer.Web/AiTrainer.Web.CoreClient.Tests/CoreClientUpdateFaissStoreTests.cs
+++ src/AiTrainer.Web/AiTrainer.Web.CoreClient.Tests/CoreClientUpdateFaissStoreTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using System.Text;
 using System.Text.Json;
+using AiTrainer.Web.Common;
 using AiTrainer.Web.Common.Configuration;
 using AiTrainer.Web.CoreClient.Clients.Concrete;
 using AiTrainer.Web.CoreClient.Models.Request;
@@ -15,7 +16,7 @@ public class CoreClientUpdateFaissStoreTests: CoreClientTestBase
 {
     public CoreClientUpdateFaissStoreTests()
     {
-        SetUpBasicHttpContext();
+        SetUpBasicHttpContext(true);
     }
 
     [Fact]
@@ -36,10 +37,9 @@ public class CoreClientUpdateFaissStoreTests: CoreClientTestBase
             .With(x => x.JsonDocStore, await JsonDocument.ParseAsync(responseMemStream))
             .Create();
         var mockedApiResponse = new CoreResponse<CoreFaissStoreResponse> { Data = response };
-        
-        var httpClient = CreateDefaultCoreClientHttpClient(HttpStatusCode.OK,
-            mockedApiResponse,
-            $"{_aiTrainerCoreConfiguration.BaseEndpoint}/api/faissrouter/updatestore");
+        var expectedUrl = $"{_aiTrainerCoreConfiguration.BaseEndpoint}/api/faissrouter/updatestore";
+        using var httpClient = CreateDefaultCoreClientHttpClient(HttpStatusCode.OK,
+            mockedApiResponse);
         var service = new CoreClientUpdateFaissStore(
             new NullLogger<CoreClientUpdateFaissStore>(),
             new TestOptions<AiTrainerCoreConfiguration>(_aiTrainerCoreConfiguration),
@@ -51,10 +51,12 @@ public class CoreClientUpdateFaissStoreTests: CoreClientTestBase
         var result = await service.TryInvokeAsync(input);
         
         //Assert
-        Assert.True(httpClient.WasExpectedUrlCalled());
+        httpClient.WasExpectedUrlCalled(expectedUrl);
+        httpClient.WasExpectedHeaderCalled(ApiConstants.CorrelationIdHeader);
+        httpClient.WasExpectedHeaderCalled(CoreClientConstants.ApiKeyHeader, _aiTrainerCoreConfiguration.ApiKey);
+        httpClient.WasExpectedHttpMethodUsed(HttpMethod.Post);
         Assert.NotNull(result);
         Assert.IsType<JsonDocument>(result.JsonDocStore);
         Assert.Equal(result.IndexFile, mockedApiResponse.Data.IndexFile);
-        
     }
 }
--- src/AiTrainer.Web/AiTrainer.Web.TestBase/AiTrainerTestBase.cs
+++ src/AiTrainer.Web/AiTrainer.Web.TestBase/AiTrainerTestBase.cs
@@ -1,4 +1,5 @@
-﻿using AutoFixture;
+﻿using AiTrainer.Web.Common;
+using AutoFixture;
 using Microsoft.AspNetCore.Http;
 using Moq;
 
@@ -6,20 +7,24 @@ namespace AiTrainer.Web.TestBase;
 
 public abstract class AiTrainerTestBase
 {
-    protected readonly Fixture _fixture = new();
+    protected static readonly Fixture _fixture = new();
     protected readonly Mock<HttpContext> _mockHttpContext = new();
     protected readonly Mock<HttpRequest> _mockHttpRequest = new();
     protected readonly Mock<HttpResponse> _mockHttpResponse = new();
     protected readonly HeaderDictionary _headerDictionary = [];
     protected readonly Mock<IHttpContextAccessor> _mockHttpContextAccessor = new();
 
-    protected void SetUpBasicHttpContext()
+    protected void SetUpBasicHttpContext(bool addCorrelationId = false)
     {
+        if (addCorrelationId)
+        {
+            _headerDictionary.Add(ApiConstants.CorrelationIdHeader, Guid.NewGuid().ToString());
+        }
         _mockHttpContext.Setup(x => x.Request).Returns(_mockHttpRequest.Object);
         _mockHttpContext.Setup(x => x.Response).Returns(_mockHttpResponse.Object);
         _mockHttpContext.Setup(x => x.Request.Headers).Returns(_headerDictionary);
         _mockHttpContext.Setup(x => x.Response.Headers).Returns(_headerDictionary);
-            
+
         _mockHttpContextAccessor.Setup(x => x.HttpContext).Returns(_mockHttpContext.Object);
     }
 }
