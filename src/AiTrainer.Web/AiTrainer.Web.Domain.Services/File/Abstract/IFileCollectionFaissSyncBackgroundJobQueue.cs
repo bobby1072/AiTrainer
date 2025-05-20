dddc68747c9f7c59977994a683dcea01de3f38eb
bobby1072 <bobby.thompson1072@gmail.com>
2025-05-20 22:50:02 +0100

    No internal

--- src/AiTrainer.Web/AiTrainer.Web.Domain.Services/File/Abstract/IFileCollectionFaissSyncBackgroundJobQueue.cs
+++ src/AiTrainer.Web/AiTrainer.Web.Domain.Services/File/Abstract/IFileCollectionFaissSyncBackgroundJobQueue.cs
@@ -5,5 +5,5 @@ namespace AiTrainer.Web.Domain.Services.File.Abstract;
 internal interface IFileCollectionFaissSyncBackgroundJobQueue: IDisposable
 {
     Task EnqueueAsync(FileCollectionFaissBackgroundJob job, CancellationToken cancellationToken = default);
-    internal Task<FileCollectionFaissBackgroundJob> DequeueAsync(CancellationToken cancellationToken = default);
+    Task<FileCollectionFaissBackgroundJob> DequeueAsync(CancellationToken cancellationToken = default);
 }
