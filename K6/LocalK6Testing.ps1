e395d2cd50008240dfd9f1914f754d176fc6d291
bobby1072 <bobby.thompson1072@gmail.com>
2025-06-05 00:15:47 +0100

    Fixed

--- K6/LocalK6Testing.ps1
+++ K6/LocalK6Testing.ps1
@@ -1,14 +1,14 @@
-docker compose -f ./../docker-compose.yml -f docker-compose.k6.local.yml -f ./../docker-compose.coreapi.yml up -d --build
+docker compose -f ./../docker-compose.yml -f docker-compose.k6.pipeline.yml -f ./../docker-compose.coreapi.yml up -d --build
 
 sleep 4
 
 
 k6 run Tests/UploadFile.js --out influxdb=http://localhost:8086/k6 --http-debug
 
-k6 run Tests/SaveFileCollection.js --out influxdb=http://localhost:8086/k6 --http-debug
+# k6 run Tests/SaveFileCollection.js --out influxdb=http://localhost:8086/k6 --http-debug
 
-k6 run Tests/GetOneLayer.js --out influxdb=http://localhost:8086/k6 --http-debug
+# k6 run Tests/GetOneLayer.js --out influxdb=http://localhost:8086/k6 --http-debug
 
-k6 run Tests/SyncFileCollectionFaiss.js --out influxdb=http://localhost:8086/k6 --http-debug
+# k6 run Tests/SyncFileCollectionFaiss.js --out influxdb=http://localhost:8086/k6 --http-debug
 
-k6 run Tests/SimilaritySearch.js --out influxdb=http://localhost:8086/k6 --http-debug
+# k6 run Tests/SimilaritySearch.js --out influxdb=http://localhost:8086/k6 --http-debug
