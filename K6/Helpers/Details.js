5626143a5f9195d0fe286cda9f5925c9e9ec5c9d
bobby1072 <bobby.thompson1072@gmail.com>
2025-06-05 22:25:54 +0100

    Samll work

--- K6/Helpers/Details.js
+++ K6/Helpers/Details.js
@@ -16,7 +16,7 @@ export const webApiEndpoint = __ENV.WEBAPIENDPOINT
   ? __ENV.WEBAPIENDPOINT
   : "http://localhost:5222";
 
-export const ThresholdsSettings = {
+export const thresholdSettings = {
   http_req_failed: [{ threshold: "rate<0.2", abortOnFail: true }],
   http_req_duration: ["p(99)<300000"],
 };
